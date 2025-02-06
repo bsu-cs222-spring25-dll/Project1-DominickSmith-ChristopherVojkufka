a16d0028975cbf8fc3d8a55cb543b1f13f3268f3
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2025-02-06 12:05:00 -0500

    established connectToWiki and main method in WikiAPI

--- src/main/java/edu/bsu/cs/WikipediaAPI.java
+++ src/main/java/edu/bsu/cs/WikipediaAPI.java
@@ -1,9 +1,33 @@
 package edu.bsu.cs;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.net.URLEncoder;
+
 public class WikipediaAPI {
 
     //https://en.wikipedia.org/wiki/Rick_Astley
     //Article Name: Rick Astley
+    public static void main() {
+        URLConnection connection = connectToWikipedia(articleName);
+        String jsonData = readJsonAsString(connection);
+        printJSON(jsonData);
+    }
+
+    private static URLConnection connectToWikipedia(String articleName) throws Exception {
+        String encodedUrlString = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles="
+                + URLEncoder.encode(articleName, Charset.defaultCharset()) +
+                "&rvprop=timestamp|user&rvlimit=4&redirects";
+        URL url = new URL(encodedUrlString);
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent",
+                "CS222FirstProject/0.1 (christopher.vojkufka@bsu.edu)");
+        connection.connect();
+        return connection;
+    }
+
     public String fetchWikipediaRevisions(String exampleArticle) {
         return null;
     }
