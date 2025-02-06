55344a1cbe06de11cb402c1c3c25de769ba9782e
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2025-02-06 11:38:45 -0500

    added wiki to test via network

--- src/main/java/edu/bsu/cs/WikipediaAPI.java
+++ src/main/java/edu/bsu/cs/WikipediaAPI.java
@@ -2,6 +2,8 @@ package edu.bsu.cs;
 
 public class WikipediaAPI {
 
+    //https://en.wikipedia.org/wiki/Rick_Astley
+    //Article Name: Rick Astley
     public String fetchWikipediaRevisions(String exampleArticle) {
         return null;
     }
