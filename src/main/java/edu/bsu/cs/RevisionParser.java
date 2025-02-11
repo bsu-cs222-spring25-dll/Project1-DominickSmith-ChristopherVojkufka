7daeee38ae49c3de8de14ad80f26e6b32dc70a95
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2025-02-11 18:22:41 -0500

    testGetRevisionsFormatter method in testParser, fails currently

--- src/main/java/edu/bsu/cs/RevisionParser.java
+++ src/main/java/edu/bsu/cs/RevisionParser.java
@@ -5,10 +5,15 @@ import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class RevisionParser {
     public String parse(InputStream testDataStream) throws IOException {
         JSONArray user = JsonPath.read(testDataStream, "$..user");
         return user.getFirst().toString();
+    }
+
+    public List<String> getRevisions(Object o) {
+        return null;
     }
 }
--- src/test/java/edu/bsu/cs/RevisionParserTest.java
+++ src/test/java/edu/bsu/cs/RevisionParserTest.java
@@ -1,10 +1,18 @@
 package edu.bsu.cs;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class RevisionParserTest {
 
@@ -13,6 +21,21 @@ public class RevisionParserTest {
         RevisionParser parser = new RevisionParser();
         InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData.json");
         String user = parser.parse(testDataStream);
-        Assertions.assertEquals("Chiswick Chap", user);
+        assertEquals("Chiswick Chap", user);
+    }
+
+    @Test
+    public void testGetRevisionsCorrectFormat() throws IOException {
+        RevisionParser parser = new RevisionParser();
+
+        String jsonContent = new String(Files.readAllBytes(Paths.get("src/test/resources/sample.json")));
+        JSONArray sampleRevisions = JsonPath.read(jsonContent, "$.query.pages.*.revisions");
+
+        List<String> revisions = parser.getRevisions(sampleRevisions.getFirst());
+        assertEquals(4, revisions.size());
+        assertEquals("1  2023-09-02T15:05:04Z  Freefry", revisions.get(0));
+        assertEquals("2  2023-09-02T15:06:03Z  Freefry", revisions.get(1));
+        assertEquals("3  2023-09-07T17:21:48Z  ModernDayTrilobite", revisions.get(2));
+        assertEquals("4  2023-09-07T18:34:43Z  Miklogfeather", revisions.get(3));
     }
 }
